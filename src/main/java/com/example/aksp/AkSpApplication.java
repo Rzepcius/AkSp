60776b1cc7825f40443efb209f355153b2d5a7b5
andrzej.rzepecki <arzepecki@bluemercury.com>
2024-10-25 11:16:07 +0200

    init

---
+++ src/main/java/com/example/aksp/AkSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.aksp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aksp/AkSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aksp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
