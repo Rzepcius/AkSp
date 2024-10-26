5eddf4177c42155d40a082fd99a70a395c994c3a
rzepciu <rzepcius@gmail.com>
2024-10-26 19:04:02 +0200

    Lekcja "Czym jest Bean"

--- src/main/java/com/example/aksp/AkSpApplication.java
+++ src/main/java/com/example/aksp/AkSpApplication.java
@@ -2,12 +2,18 @@ package com.example.aksp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class AkSpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AkSpApplication.class, args);
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void get(){
+        System.out.println("Hello World");
     }
 
 }
---
+++ src/main/java/com/example/aksp/spTest/CarService.java
@@ -0,0 +1,33 @@
+package com.example.aksp.spTest;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> carList;
+
+    public CarService() {
+        Car car1 = new Car("BMW", "i8");
+        Car car2 = new Car("Fiat", "126p");
+        Car car3 = new Car("Polonez", "Caro");
+        carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void showCar() {
+        carList.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        CarService carService = new CarService();
+        carService.showCar();
+    }
+}
