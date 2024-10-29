fce86d3601f7f7b6743185171fdf77549f2e5a57
rzepciu <rzepcius@gmail.com>
2024-10-29 18:28:14 +0100

    Lekcja bonus logowanie

--- src/main/java/com/example/aksp/spTest/ShopBasic.java
+++ src/main/java/com/example/aksp/spTest/ShopBasic.java
@@ -1,5 +1,7 @@
 package com.example.aksp.spTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
@@ -8,9 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("Basic")
 public class ShopBasic {
-
+    Logger logger = LoggerFactory.getLogger(ShopBasic.class);
     @EventListener(ApplicationReadyEvent.class)
     public void get(){
         System.out.println(this.getClass());
+        logger.warn(this.getClass()+"!!!!");
     }
 }
--- src/main/java/com/example/aksp/spTest/ShopPremium.java
+++ src/main/java/com/example/aksp/spTest/ShopPremium.java
@@ -1,5 +1,7 @@
 package com.example.aksp.spTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
@@ -9,8 +11,11 @@ import org.springframework.stereotype.Component;
 @Profile("Premium")
 public class ShopPremium {
 
+    Logger logger = LoggerFactory.getLogger(ShopPremium.class);
+
     @EventListener(ApplicationReadyEvent.class)
     public void get(){
         System.out.println(this.getClass());
+        logger.warn(this.getClass()+"!!!!");
     }
 }
