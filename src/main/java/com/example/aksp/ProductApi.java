284ce1ab049232e9e4a0a0d209d5544b891536ed
rzepciu <rzepcius@gmail.com>
2024-11-05 20:19:14 +0100

    Parametryzacja metod webowych

--- src/main/java/com/example/aksp/ProductApi.java
+++ src/main/java/com/example/aksp/ProductApi.java
@@ -15,26 +15,46 @@ public class ProductApi {
 //    @RequestMapping(value = "/products",method = RequestMethod.GET)
 //    @RequestMapping(method = RequestMethod.GET)
 //    @GetMapping("/get") -- http://localhost:8080/products/get
+
+//    Parametryzacja przez Argument
+//    @GetMapping
+//    public String getProducts(@RequestParam String name,
+//                              @RequestParam(required = false,defaultValue = "") String surName){
+
+//    Parametryzacja przez Ścieżkę, najczesciej wybierany
+//    @GetMapping("/{name}")
+//    public String getProducts(@PathVariable String name){
+
+//    Parametryzacja przez Header
+//    @GetMapping
+//    public String getProducts(@RequestHeader String name){
+//    Parametryzacja przez Body
+//    @GetMapping
+//    public String getProducts(@RequestBody String name){
+
+    //    Połączone
     @GetMapping
-    public String getProducts(){
-        return "Hel with get";
+    public String getProducts(@RequestParam String name,
+                              @RequestHeader(required = false, defaultValue = "") String surName) {
+        return "Hel with get to: " + name + " " + surName;
+//        return "Hel with get to: "+name;
     }
 
-//    @PostMapping("/products")
+    //    @PostMapping("/products")
     @PostMapping
-    public String addProduct(){
+    public String addProduct() {
         return "Hel with post";
     }
 
-//    @PutMapping("/products")
+    //    @PutMapping("/products")
     @PutMapping
-    public String modProduct(){
+    public String modProduct() {
         return "Hel with put";
     }
 
-//    @DeleteMapping("/products")
+    //    @DeleteMapping("/products")
     @DeleteMapping
-    public String removeProduct(){
+    public String removeProduct() {
         return "Hel with delete";
     }
 }
